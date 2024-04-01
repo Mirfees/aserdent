761e49cc28f98e16926eee1fefba6d08cd3baa18
Miron Fissenko <mirfees@gmail.com>
2024-04-01 21:24:27 +0500

    Сделал страницу отзывы, технологии, цены, блог, внутренняя страница блога

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //end BURGER-MENU
 
+    //begin TABS
+    const pricesTabsContainer = document.querySelector('.prices__tabs-container');
+    const tabTitleClass = 'prices__tab-title';
+    const tabClass = 'prices__tab';
+    if (pricesTabsContainer) {
+        pricesTabsContainer.addEventListener('click', (e) => {
+            let target = e.target;
+
+            if(target.closest(`.${tabTitleClass}`)) {
+                let tabTitle = (target.closest(`.${tabTitleClass}`));
+                let tabTitles = pricesTabsContainer.querySelectorAll(`.${tabTitleClass}`);
+                let tabId = tabTitle.dataset.tabId;
+                let tab = pricesTabsContainer.querySelector('#' + tabId);
+                let tabs = pricesTabsContainer.querySelectorAll(`.${tabClass}`);
+
+                tabTitles.forEach(title => {
+                    title.classList.remove('active');
+                });
+
+                tabs.forEach(tab => {
+                    tab.classList.remove('active');
+
+                    if (tab.id === tabId) {
+                        tab.classList.add('active');
+                    }
+                });
+
+                tabTitle.classList.add('active');
+            }
+        });
+    }
+
+    //end TABS
+
     //begin SLIDERS
 
     const mainPageSlider = new Swiper('.main-page-slider__swiper', {
@@ -32,6 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
         loop: true,
         slidesPerView: 2,
         spaceBetween: 20,
+        autoplay: true,
 
         pagination: {
             el: '.slider-dentists__pagination',
@@ -48,19 +83,19 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 
-    const sliderFeedback = new Swiper('.feedback__swiper', {
+    const sliderFeedback = new Swiper('.slider-feedback__swiper', {
         direction: 'horizontal',
         loop: true,
         slidesPerView: 3,
         spaceBetween: 20,
 
         pagination: {
-            el: '.feedback__pagination',
+            el: '.slider-feedback__pagination',
         },
 
         navigation: {
-            nextEl: '.feedback__next',
-            prevEl: '.feedback__prev',
+            nextEl: '.slider-feedback__next',
+            prevEl: '.slider-feedback__prev',
         },
 
         breakpoints: {
@@ -118,6 +153,56 @@ document.addEventListener('DOMContentLoaded', function () {
             1500: {
                 slidesPerView: 4,
             },
+            1000: {
+                slidesPerView: 3,
+            },
+            576: {
+                slidesPerView: 2,
+            },
+
+            0: {
+                slidesPerView: 1,
+            },
+        },
+    });
+
+    const sliderLicense = new Swiper('.slider-license .swiper', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+
+        navigation: {
+            nextEl: '.slider-license .swiper-button-next',
+            prevEl: '.slider-license .swiper-button-prev',
+        },
+
+        breakpoints: {
+            1000: {
+                slidesPerView: 3,
+            },
+            576: {
+                slidesPerView: 2,
+            },
+
+            0: {
+                slidesPerView: 1,
+            },
+        },
+    });
+
+    const sliderPosts = new Swiper('.slider-posts .swiper', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+
+        navigation: {
+            nextEl: '.slider-posts .swiper-button-next',
+            prevEl: '.slider-posts .swiper-button-prev',
+        },
+
+        breakpoints: {
             1000: {
                 slidesPerView: 3,
             },
